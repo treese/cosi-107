73c5afda231b90f958a43dc3335a86a1d8f7b9a9
Win Treese <win@treese.org>
2023-01-26 23:04:05 -0500

    Add python template for Lab 2

---
+++ lab-02/python-template.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+#
+# <<portions in angle brackets like this can be replaced by your
+# relevant code>>
+#
+# <<you can remove any of these comments or rewrite things to describe
+# what you do>>
+# <<lines should less than 80 characters long>>
+# <<indenting each level of block should be 4 spaces>>
+#
+# <<What the program does>>
+#
+# Written by: <<your-name>>
+
+# <<Put all import statements here>>
+
+# <<function definitions>>
+
+# <<example function definition>
+def scan_port(ip_addr, port):
+    """Scan a single port on a host.
+    Arguments:
+    ip_addr -- The IP address of the host. Should already be in
+               numeric "dotted quad" form.
+    port -- Integer value for a port number.
+    """
+    pass
+
+
+def main():
+    # <<Parse command line arguments>>
+    # <<call a small number of functions to do the work>
+    # <<the "pass" is just here so that the main function isn't empty.
+    pass
+
+
+# <<This code causes the main function to be called if this module is invoked as a program.>>
+
+
+if __name__ == "__main__":
+    main()
